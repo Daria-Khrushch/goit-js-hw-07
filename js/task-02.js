886866f073708c99df467068ea2e1872f6b7cdef
Daria Khrushch <Daria140694@gmail.com>
2021-04-24 15:41:43 +0300

    task3

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,9 @@ const ingredients = [
   'Приправы',
 ];
 
-ingredients.forEach(function (number, index) {
+ingredients.forEach(function (item, index) {
     const listEl = document.createElement('li');
     const navEl = document.querySelector('ul');
-    listEl.textContent = `${number}`;
+    listEl.textContent = `${item}`;
     console.log(navEl.appendChild(listEl));
  });
