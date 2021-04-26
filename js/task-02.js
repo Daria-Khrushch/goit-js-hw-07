df14ce3b326b40ddb64963defc50264771467e16
Daria Khrushch <Daria140694@gmail.com>
2021-04-26 16:51:22 +0300

    исправила task 6

--- js/task-02.js
+++ js/task-02.js
@@ -7,9 +7,16 @@ const ingredients = [
   'Приправы',
 ];
 
+// ingredients.forEach(function (item, index) {
+//     const listEl = document.createElement('li');
+//     const navEl = document.querySelector('ul');
+//     listEl.textContent = item;
+//     console.log(navEl.appendChild(listEl));
+//  });
+
 ingredients.forEach(function (item, index) {
-    const listEl = document.createElement('li');
-    const navEl = document.querySelector('ul');
-    listEl.textContent = `${item}`;
-    console.log(navEl.appendChild(listEl));
- });
+  console.log(item);
+  const navEl = document.querySelector('ul');
+  // console.log(navEl.appendChild(item));
+
+})
--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,19 @@
 const input = document.querySelector('input');
 input.addEventListener('blur', onInputBlur);
+const lengthEl = input.dataset['length'];
+console.log(lengthEl);
 
 function onInputBlur(event) {
-    if (input.value.length === 6) {
+    const trueLength = input.value.length;
+    console.log(trueLength);
+    if (trueLength === Number(lengthEl)) {
+        console.log('ok');
         input.classList.remove('invalid');
         input.classList.add('valid');
     } else {
         input.classList.remove('valid');
         input.classList.add('invalid');
+        console.log('neok');
     };
 }
 
