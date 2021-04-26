eef94b2a979209f64375226cdb6fd05f9b36ea91
Daria Khrushch <Daria140694@gmail.com>
2021-04-26 15:53:42 +0300

    исправления в task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,9 @@
 const navItemEl = document.querySelectorAll('.item');
 console.log(`В списке ${navItemEl.length} категории.`);
 
-const titleAnimals = document.querySelector('.title_animals');
-console.log(`Категория: ${titleAnimals.textContent}`);
-const animalsEl = document.querySelectorAll('.animals li');
-console.log(`Количество элементов: ${animalsEl.length}`);
+const itemEl = document.querySelectorAll('.item ul');
 
-const titleFood = document.querySelector('.title_food');
-console.log(`Категория: ${titleFood.textContent}`);
-const foodEl = document.querySelectorAll('.food li');
-console.log(`Количество элементов: ${foodEl.length}`);
-
-const titleTechnologies = document.querySelector('.title_technologies');
-console.log(`Категория: ${titleTechnologies.textContent}`);
-const technologiesEl = document.querySelectorAll('.technologies li');
-console.log(`Количество элементов: ${technologiesEl.length}`);
+itemEl.forEach(function (item, index) {
+    console.log(`Категория: ${item.previousElementSibling.textContent}`);
+    console.log(`Количество элементов: ${item.children.length}`);
+})
